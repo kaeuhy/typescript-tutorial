9e195f77330da46e01356dbf1fe2b700cfb3bbd6
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-07-02 14:22:14 +0900

    typescript에서의 object

---
+++ section2/src/chapter3.ts
@@ -0,0 +1,18 @@
+//object
+let user: {
+    id: number;
+    name: string;
+} = {
+    id: 1,
+    name: "강은현",
+};
+
+let dog: {
+    name: string;
+    color: string;
+} = {
+    name: "돌돌이",
+    color: "brown",
+};
+
+user.id;
